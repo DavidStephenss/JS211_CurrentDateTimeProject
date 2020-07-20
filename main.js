152843dc79ddef6c08863b419ec25913a4d654a7
DavidStephenss <dvdstephensd@gmail.com>
2020-07-19 23:15:00 -0500

    yay

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 //          PART ONE
 // ***************************
 // Write a JavaScript program to display the current day and time, start with:
-console.log(new Date)
+let ourNumberToString = null
 
 const displayDate = () => {
   const currentDate = new Date()
@@ -13,13 +13,22 @@ const displayDate = () => {
   document.getElementById("display-element").innerHTML = currentDate;
 }
  
-
 // Write a JavaScript program to convert a number to a string.
 
-
+const convertNumToString = (num) => {
+  ourNumberToString = num
+  
+  return document.getElementsById("number-to-string-element").innerHTML =
+  ourNumberToString + " is a " + typeof ourNumberToString
+}
 
 // Write a JavaScript program to convert a string to the number.
+const convertStringToNum = (string) => {
+  return parseInt(string)
+}
 
+console.log(convertStringToNum("5"))
+console.log(convertStringToNum("hello"))
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
@@ -29,23 +38,50 @@ const displayDate = () => {
   // * Number
   // * NaN
   // * String
-  
 
+  function isDataType(arg) {
+    return typeof (arg)
+  }
   
 // Write a JavaScript program that adds 2 numbers together.
-
+const myFunction = (numX, numY) => {
+  sum = (numX + numY);
+  return (sum);
+}
+console.log(myFunction(4,5))
 
 
 // Write a JavaScript program that runs only when 2 things are true.
-
-
+const ifBothAreTrue = (thing1, thing2) => {
+  if( thing1 && thing2 ) {
+    return true
+  } else {
+    return false
+  }
+}
 
 // Write a JavaScript program that runs when 1 of 2 things are true.
-
-
+const ifEitherAreTrue = (thing1, thing2) => {
+  if( thing1 || thing2 ) {
+    return true
+  } else {
+    return false
+  }
+}
 
 // Write a JavaScript program that runs when both things are not true.  
+const ifNeitherAreTrue = (thing1, thing2) => {
+  if( !thing1 && !thing2 ) {
+    return true
+  } else {
+    return false
+  }
+}
 
+console.log("*******")
+console.log(ifNeitherAreTrue(0, 0))
+console.log(ifNeitherAreTrue(1, 0))
+console.log(ifNeitherAreTrue(1, 1))
 // ***************************
 //         PART TWO
 // ***************************
